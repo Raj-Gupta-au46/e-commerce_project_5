a38ea200ddd7df1a6f8083f83d6be8c9adeda480
shivamgupta83 <shivam8359083814@gmail.com>
2022-12-20 00:13:53 +0530

    done

--- demoproject5/src/routes/route.js
+++ demoproject5/src/routes/route.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const userController= require("../controller/userController")
+const {Authentication,Authenthrization}= require("../auth/mid")
 const router = express.Router()
 
 
@@ -13,3 +15,25 @@ const router = express.Router()
 
 router.post("/register",createUser)
 router.post('/login', loginUser)
+router.get("/user/:userId/profile",Authentication,userController.getUserData)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.all("/*",(_,res)=>{
+    return res.status(400).send({status:false,msg:"your end point is not correct"})
+})
